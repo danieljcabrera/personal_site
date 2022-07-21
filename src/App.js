f0cae45f7273eec433f8f4222b6cc61a1da28471
danieljcabrera <djcabrera@roadrunner.com>
2022-07-20 20:33:21 -0700

    Updated Killa pic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import killua from "./KilluaProfilePic.jpg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={"KilluaProfilePic.jpg"} alt="logo" />
-        <p>Hello World!</p>
+        <img src={killua} alt="logo" />
+        <p>{"Hi :)"}</p>
       </header>
     </div>
   );
